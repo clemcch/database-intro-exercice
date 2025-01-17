a30ac403e05dcbebf052d2ae5212b0235d7c3f8c
clemcch <clement.coucharriere@eemi.com>
2025-01-17 12:17:02 +0100

    Add files via upload

--- Exercice2/Ex1.sql
+++ Exercice2/Ex1.sql
@@ -1,1 +1,20 @@
 SELECT firstname, lastname FROM authors;
+
+
+
+
+
+SELECT name FROM editors WHERE name LIKE '%,%';
+
+
+
+
+
+SELECT * FROM loans
+WHERE return_date < CURDATE() AND is_returned = FALSE;
+
+
+
+
+
+afficher le titre des livres dont le titre comporte moins de 50 caractères
